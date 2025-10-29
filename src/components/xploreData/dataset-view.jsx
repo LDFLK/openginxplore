2b3be052ccfd6f37c96851ac7724eeb57c9e4bcd
Chamith Chanuka <chamithroxmpc@gmail.com>
2025-10-29 21:48:39 +0530

    Dataset available message added

--- src/components/xploreData/dataset-view.jsx
+++ src/components/xploreData/dataset-view.jsx
@@ -3,28 +3,22 @@ import { useEffect, useState } from "react";
 import { ClipLoader } from "react-spinners";
 import apiData from "../../services/xploredataServices";
 import { ChartVisualization } from "./chart-visualization";
-import { useSearchParams } from "react-router-dom";
 
 export function DatasetView({ data }) {
   const datasets = data;
 
   const [loadingDatasetId, setLoadingDatasetId] = useState(null);
   const [selectedDataset, setSelectedDataset] = useState(null);
-  const [selectedYear, setSelectedYear] = useState(
-    Object.keys(data).length > 0 ? Object.keys(data)[0] : ""
-  );
+  const [selectedYear, setSelectedYear] = useState("");
   const [dataCache, setDataCache] = useState({});
   const [years, setYears] = useState([]);
-  const [searchParams] = useSearchParams();
-
-  console.log(searchParams)
 
   useEffect(() => {
     if (!datasets) return;
 
-    console.log(searchParams);
-    const startDate = searchParams.get("startDate");
-    const endDate = searchParams.get("endDate");
+    const params = new URLSearchParams(window.location.search);
+    const startDate = params.get("startDate");
+    const endDate = params.get("endDate");
 
     if (!startDate || !endDate) {
       console.warn("Missing startDate or endDate in URL params");
@@ -32,16 +26,13 @@ export function DatasetView({ data }) {
     }
     const startYear = startDate.split("-")[0];
     const endYear = endDate.split("-")[0];
-    console.log("start year : ", startYear);
-    console.log("end year : ", endYear);
 
     const filteredYears = Object.keys(datasets).filter(
       (year) => year >= startYear && year <= endYear
     );
-    console.log("filtered years : ", filteredYears);
-
-    setYears(Object.keys(datasets));
-  }, [datasets, searchParams.toString()]);
+    setSelectedDataset(null);
+    setYears(filteredYears);
+  }, [datasets, window.location.search]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -81,41 +72,47 @@ export function DatasetView({ data }) {
   return (
     <div className="p-4 md:p-6 space-y-6 w-full">
       {/* Dataset Info */}
-      <div className="space-y-2">
-        <h2 className="text-xl md:text-2xl font-bold text-gray-300">
-          {datasets[selectedYear][0]?.nameExact || "Select a Dataset"}
-        </h2>
-        <div className="flex flex-col md:flex-row gap-4 md:gap-6 text-sm text-gray-400">
-          <p>
-            <span className="font-semibold">Published By : </span>{" "}
-            {datasets[selectedYear][0]?.source || "—"}
-          </p>
+      {years && years.length > 0 && (
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-bold text-gray-300">
+            {datasets[selectedYear]
+              ? datasets[selectedYear][0]?.nameExact
+              : "Select a Dataset"}
+          </h2>
+          <div className="flex flex-col md:flex-row gap-4 md:gap-6 text-sm text-gray-400">
+            <p>
+              <span className="font-semibold">Published By : </span>{" "}
+              {datasets[selectedYear] ? datasets[selectedYear][0]?.source : "—"}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
 
       {/* Year Selector */}
-      <div className="w-full flex justify-end">
-        <select
-          className="w-2/9 bg-gray-900 text-gray-100 border border-gray-700 rounded-lg px-3 py-2 
+      {years && years.length > 0 && (
+        <div className="w-full flex justify-end">
+          <select
+            className="w-2/9 bg-gray-900 text-gray-100 border border-gray-700 rounded-lg px-3 py-2 
                    focus:outline-none 
                    hover:border-gray-500 transition"
-          value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
-        >
-          <option value="" hidden>
-            Select Year
-          </option>
-          {years.map((year) => (
-            <option
-              key={year}
-              value={year}
-              className="bg-gray-900 text-gray-100"
-            >
-              {year}
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(e.target.value)}
+          >
+            <option value="" hidden>
+              Select Year
             </option>
-          ))}
-        </select>
-      </div>
+            {years.map((year) => (
+              <option
+                key={year}
+                value={year}
+                className="bg-gray-900 text-gray-100"
+              >
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
 
       {/* Dataset Visualization */}
       <div className="border border-gray-700 rounded-xl p-4 shadow-sm bg-gray-900">
@@ -139,9 +136,15 @@ export function DatasetView({ data }) {
                 title={selectedDataset.attributeName}
               />
             </div>
+          ) : years && years.length == 0 && Object.keys(datasets).length > 0 ? (
+            <div className="block justify-center items-center">
+              <p className="text-gray-500 italic text-center">
+                No data yet! But you have data for {Object.keys(datasets)}
+              </p>
+            </div>
           ) : (
             <p className="text-gray-500 italic text-center">
-              Please select a dataset year.
+              Select a year to view the dataset
             </p>
           )}
         </div>
