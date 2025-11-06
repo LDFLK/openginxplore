82517bcf0030f9df630babd2b8cb306376df4fea
Chamith Chanuka <chamithroxmpc@gmail.com>
2025-11-06 16:05:35 +0530

    config file edited

--- public/config.js
+++ public/config.js
@@ -4,14 +4,11 @@
 
 //example
 // window.configs = {
-//     apiUrl : null ,
-//     version : "rc-0.1.0",
-//     apiUrlService : "http://127.0.0.1:8000"
+//     apiUrl : "",
+//     apiUrlData: "",
+//     feedbackFormUrl: "",
+//     version : "rc-0.1.0"
 // };
-
-window.configs = {
-    feedbackFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSc-gXC_h30DueTWzlKMI4zgNXwX9ae_88FF37G1SpSaILEbng/viewform?usp=dialog"
-}
 
 // get the data to the relevant component using ->
 // example
