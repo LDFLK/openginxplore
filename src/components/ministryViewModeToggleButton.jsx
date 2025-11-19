df502e9286451d56329d63665302c7e5939f8e9b
sehansi-9 <sehansi.wsmp9@gmail.com>
2025-11-19 10:55:48 +0530

    improve view mode toggle button

--- src/components/ministryViewModeToggleButton.jsx
+++ src/components/ministryViewModeToggleButton.jsx
@@ -1,31 +1,40 @@
 import { PiGraph } from "react-icons/pi";
 import { MdGridOn } from "react-icons/md";
-import { useThemeContext } from "../themeContext";
 
-export default function MinistryViewModeToggleButton({
-  viewMode,
-  setViewMode,
-}) {
+export default function MinistryViewModeToggleButton({ viewMode, setViewMode }) {
+  const isGraph = viewMode === "Graph";
+
   return (
     <div className="flex justify-end">
-      <button
-        className={`flex justify-center items-center py-3 px-4 cursor-pointer transition-all duration-200 ease-in-out rounded-l-sm ${
-          viewMode == "Grid" ? "text-primary bg-foreground/10" : "text-primary"
-        }`}
-        onClick={() => setViewMode("Grid")}
-      >
-        <MdGridOn className="mr-2 text-xl" />
-        List
-      </button>
-      <button
-        className={`flex justify-center items-center py-3 px-4 cursor-pointer transition-all duration-200 ease-in-out rounded-r-sm ${
-          viewMode == "Graph" ? "text-primary bg-foreground/10" : "text-primary"
-        }`}
-        onClick={() => setViewMode("Graph")}
-      >
-        <PiGraph className="mr-2 text-xl" />
-        Graph
-      </button>
+      <div className="relative flex bg-transparent border-2 border-foreground/20 rounded-md w-48 h-14.5 select-none">
+        <div
+          className={`absolute top-0 bottom-0 w-1/2 rounded-md transition-all duration-300
+    ${isGraph ? "border-l-2 border-foreground/20" : "border-r-2 border-foreground/20"}
+  `}
+          style={{ left: isGraph ? "50%" : "0", borderLeftColor: isGraph && "border-2 border-foreground/20", borderRightColor: !isGraph && "border-2 border-foreground/20" }}
+        />
+
+        <button
+          onClick={() => setViewMode("Grid")}
+          className={`relative z-10 flex items-center justify-start gap-1 w-1/2 py-3 px-3  hover: cursor-pointer  text-md font-medium transition-all rounded-md
+            ${!isGraph ? "text-primary/80 bg-foreground/10" : "text-primary/50"}
+          `}
+        >
+          <MdGridOn className={`text-lg transition-all ${!isGraph ? "opacity-100" : "opacity-50"}`} />
+          List
+        </button>
+
+        <button
+          onClick={() => setViewMode("Graph")}
+          className={`relative z-10 flex items-center justify-center hover: cursor-pointer gap-1 w-1/2 py-3 px-3 text-md font-medium transition-all rounded-md
+            ${isGraph ? "text-primary/80 bg-foreground/10" : "text-primary/50"}
+          `}
+        >
+          <PiGraph className={`text-lg transition-all ${isGraph ? "opacity-100" : "opacity-50"}`} />
+          Graph
+        </button>
+
+      </div>
     </div>
   );
 }
