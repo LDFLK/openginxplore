6f88d52715a1a84f96ba79f3c0de0979a97427a2
sehansi-9 <sehansi.wsmp9@gmail.com>
2025-11-19 15:27:43 +0530

    style: improve visibility of ministry view mode toggle button

--- src/components/ministryViewModeToggleButton.jsx
+++ src/components/ministryViewModeToggleButton.jsx
@@ -6,10 +6,10 @@ export default function MinistryViewModeToggleButton({ viewMode, setViewMode }) 
 
   return (
     <div className="flex justify-end w-full sm:w-auto">
-      <div className="relative flex bg-transparent border-2 border-foreground/20 rounded-md w-38 h-10 select-none">
+      <div className="relative flex bg-transparent border border-foreground/80 rounded-md w-38 h-10 select-none">
         <div
           className={`absolute top-0 bottom-0 w-1/2 rounded-md transition-all duration-300 ${
-            isGraph ? "border-l-2 border-foreground/20" : "border-r-2 border-foreground/20"
+            isGraph ? "border-l-1 border-foreground/80" : "border-r-1 border-foreground/80"
           }`}
           style={{
             left: isGraph ? "50%" : "0",
@@ -25,7 +25,7 @@ export default function MinistryViewModeToggleButton({ viewMode, setViewMode }) 
             !isGraph ? "text-primary/80 bg-foreground/10" : "text-primary/50"
           }`}
         >
-          <MdGridOn className={`text-base transition-all ${!isGraph ? "opacity-100" : "opacity-50"}`} />
+          <MdGridOn className={`text-base transition-all ${!isGraph ? "opacity-100" : "opacity-80"}`} />
           List
         </button>
 
@@ -36,7 +36,7 @@ export default function MinistryViewModeToggleButton({ viewMode, setViewMode }) 
             isGraph ? "text-primary/80 bg-foreground/10" : "text-primary/50"
           }`}
         >
-          <PiGraph className={`text-base transition-all ${isGraph ? "opacity-100" : "opacity-50"}`} />
+          <PiGraph className={`text-base transition-all ${isGraph ? "opacity-100" : "opacity-80"}`} />
           Graph
         </button>
       </div>
