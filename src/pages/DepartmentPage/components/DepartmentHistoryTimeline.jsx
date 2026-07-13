94fc99e493077fa95e7c3a478400d0315b55c226
zaeema-n <zaeemanashath@gmail.com>
2026-07-13 15:43:37 +0530

    style: increase padding in DepartmentHistoryTimeline for improved layout

--- src/pages/DepartmentPage/components/DepartmentHistoryTimeline.jsx
+++ src/pages/DepartmentPage/components/DepartmentHistoryTimeline.jsx
@@ -58,7 +58,7 @@ const DepartmentHistoryTimeline = ({ selectedDepartment }) => {
                     color: isDark ? "#f8f8f8" : "#0b0b0b",
                     border: `1px solid ${isDark ? "#364153" : "#dbdbdb"}`,
                     boxShadow: "none",
-                    padding: "10px",
+                    padding: "12px",
                     paddingTop: "0px",
                     marginBottom: "25px",
                     marginTop: "15px",
